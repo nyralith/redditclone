554f2810ca2e46be21ee350d3ab38a9a092eeabc
nyralith <walkarel123@hotmail.com.ar>
2021-09-23 19:14:18 -0300

    Added MikroORM TypeGraphQL Crud

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
 const core_1 = require("@mikro-orm/core");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
+const apollo_server_core_1 = require("apollo-server-core");
+const posts_1 = require("./resolvers/posts");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
+    const app = (0, express_1.default)();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [hello_1.HelloResolver, posts_1.PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+        plugins: [(0, apollo_server_core_1.ApolloServerPluginLandingPageGraphQLPlayground)()],
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000");
+    });
 };
 main().catch((err) => {
     console.log(err);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { PostResolver } from "./resolvers/posts";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
-  //  const posts = await orm.em.find(Post, {})
-  //  console.log(posts)
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
